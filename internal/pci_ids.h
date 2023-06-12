720140d0a9bbdc1e38fb7bb8f6063da29fae11ee
YeeLi <seven.yi.lee@gmail.com>
2023-06-12 10:27:32 +0800

    add #ifndef and #define about DEVICE_ID into internal/pci_ids.h for the old kernel like kernel-5.4 which doesn't have

--- internal/pci_ids.h
+++ internal/pci_ids.h
@@ -7,6 +7,19 @@
  *	Do not add new entries to this file unless the definitions
  *	are shared between multiple drivers.
  */
+
+#ifndef PCI_DEVICE_ID_AMD_19H_M10H_DF_F3
+#define PCI_DEVICE_ID_AMD_19H_M10H_DF_F3 0x1B40
+#endif
+
+#ifndef PCI_DEVICE_ID_AMD_19H_M40H_DF_F3
+#define PCI_DEVICE_ID_AMD_19H_M40H_DF_F3 0x167C
+#endif
+
+#ifndef PCI_DEVICE_ID_AMD_19H_M50H_DF_F3
+#define PCI_DEVICE_ID_AMD_19H_M50H_DF_F3 0x166D
+#endif
+
 #ifndef _INTERNAL_PCI_IDS_H
 #define _INTERNAL_PCI_IDS_H
 
